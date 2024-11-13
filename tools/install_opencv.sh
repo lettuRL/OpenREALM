74e2f02d136bf87e91bc7960d90beeed7ffe0b66
乐途 <43346085+lettuRL@users.noreply.github.com>
2024-11-13 16:13:27 +0800

    Update install_opencv.sh

--- tools/install_opencv.sh
+++ tools/install_opencv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-cd ~ && git clone -b 3.3.1 https://github.com/opencv/opencv.git \
+cd ~ && git clone -b 3.2.0 https://github.com/opencv/opencv.git \
 && cd ~/opencv && mkdir build && cd build \
 && cmake -D WITH_CUDA=OFF -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local .. \
 && make && sudo make install
